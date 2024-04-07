02e6b972e7f8fb146cd1c31a78c0a31c953a1a6f
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-04-07 20:33:02 -0300

    Id comentado

--- TP01/aeds3/DeletedIndexRegister.java
+++ TP01/aeds3/DeletedIndexRegister.java
@@ -6,7 +6,8 @@ import java.io.IOException;
 
 public class DeletedIndexRegister implements RegistroHashExtensivel {
 
-    private int ID; // id do elemento deletado
+    //private int ID; 
+    // id do elemento deletado
     private short length; // tamanho do arquivo deletado
     private long position; // posição do arquivo deletado no arquivo
     final private int TAMANHO = 14; // tamanho do int(4) short(2) long(8)
@@ -30,6 +31,10 @@ public class DeletedIndexRegister implements RegistroHashExtensivel {
         this.position = position;
     }
 
+    public String toString() { 
+        return "{\n     Tamanho: " + length + "\n       Posição: " + position + "\n}";
+    }
+
     @Override
     public short size() {
         return TAMANHO;
@@ -38,7 +43,7 @@ public class DeletedIndexRegister implements RegistroHashExtensivel {
     public byte[] toByteArray() throws IOException {
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
         DataOutputStream data = new DataOutputStream(bytes);
-        data.writeInt(ID);
+        //data.writeInt(ID);
         data.writeShort(length);
         data.writeLong(position);
         return bytes.toByteArray();
@@ -47,7 +52,7 @@ public class DeletedIndexRegister implements RegistroHashExtensivel {
     public void fromByteArray(byte[] bytes) throws IOException {
         ByteArrayInputStream arrayInput = new ByteArrayInputStream(bytes);
         DataInputStream data = new DataInputStream(arrayInput);
-        this.ID = data.readInt();
+        //this.ID = data.readInt();
         this.length = data.readShort();
         this.position = data.readLong();
     }
