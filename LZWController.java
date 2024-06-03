dcd575591535e55d60c7da78631a503dfbd4114b
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-06-03 19:21:37 -0300

    decodificação teste

--- LZWController.java
+++ LZWController.java
@@ -57,27 +57,62 @@ public class LZWController {
             String dirOut = simple.format(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()));
             File file = new File(dir + "/" + dirOut);
             file.mkdir();
-            System.out.println(dir + "/" + dirOut);
             readAndWrite(nome, bytesByExecution, dirOut);
         }
     }
 
-    public static void makeMenu() {
+    public void decompressData(String filePath, int bytesByExecution) throws Exception {
+        try {
+            RandomAccessFile file = new RandomAccessFile(filePath, "rw");
+            byte[] conteudo = new byte[10];
+            System.out.println("\n" + filePath);
+            while (file.getFilePointer() < file.length()) {
+                file.read(conteudo);
+                for (byte b : conteudo) {
+                    System.out.print(b);
+                }
+            }
+            System.out.println();
+            System.out.println();
+            file.close();
+        } catch (Exception e) {
+            throw new Exception("Erro ao abrir o arquivo para decodificação!");
+        }
+    }
+
+    public void decompressData(String dirBackUp, String dirOut, int bytesByExecution) throws Exception {
+        String[] files = (new File(dirBackUp)).list();
+        for (String file : files) {
+            decompressData(dirBackUp + "/" + file, 10);
+            RandomAccessFile fileOut = new RandomAccessFile(dirOut + "/" + file.substring(0, file.length()-4), "rw");
+            
+            byte[] conteudo = new byte[(int) fileOut.length()];
+            fileOut.read(conteudo);
+            for (byte b : conteudo) {
+                System.out.print(b);
+            }
+            System.out.println();
+            fileOut.close();
+        }
+    }
+
+    public void makeMenu() throws Exception {
         System.out.println("Choose a backup:");
-        String[] dirList = (new File("backup")).list();
+        String[] dirList = (new File(dir)).list();
         for(int i = 0; i < dirList.length; i++) {
-            System.out.println("    " + i + " " + dirList[i]);
+            System.out.println("    " + i + ": " + dirList[i]);
         }
         System.out.print("Resposta: ");
         Scanner sc = new Scanner(System.in);
         int res = sc.nextInt();
+        decompressData(dir + "/" + dirList[res], "dados", 10);
         sc.close();
     }
 
     public static void main(String[] args) throws Exception {
         LZWController controller = new LZWController("backup");
-        makeMenu();
-        controller.compressData(10);
+        controller.makeMenu();
+        //controller.compressData(10);
     }
 
 }
